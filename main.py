a3c737b6689ede073bcccab8b3b108024bd93065
jackvincent <vincent.jack@icloud.com>
2024-06-05 21:51:40 +0100

    Add ability to create new tasks and save them

--- main.py
+++ main.py
@@ -1,27 +1,24 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Boolean, create_engine
+import pandas
 import os
-
-
-class Base(DeclarativeBase):
-    pass
-
 
 app = Flask(__name__)
 Bootstrap5(app)
-
-db = SQLAlchemy(model_class=Base)
 app.config['SECRET_KEY'] = os.environ.get("FLASK_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///items.db"
-db.init_app(app)
+
+df = pandas.read_csv("instance/task-data.csv")
 
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template("index.html")
+    if "add" in request.form:
+        text = request.form.get("task-name")
+        df.loc[len(df.index)] = {"name": text, "complete": False}
+        df.to_csv("instance/task-data.csv", index=False)
+
+    return render_template("index.html", tasks=df)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
